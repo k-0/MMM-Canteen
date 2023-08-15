3f9582343ef82600a2a883f6601a6aef80c62fe0
Kristjan SCHMIDT <35647502+KristjanESPERANTO@users.noreply.github.com>
2023-08-15 23:18:45 +0200

    Linting, var -> let/const, console.log -> Log.*

--- node_helper.js
+++ node_helper.js
@@ -1,20 +1,18 @@
-var NodeHelper = require('node_helper');
-const https = require('https');
-var moment = require('moment');
-
+const NodeHelper = require("node_helper");
+const request = require("request");
+const moment = require("moment");
+const Log = require("logger");
 
 module.exports = NodeHelper.create({
-
-  start: function() {
-    console.log("Starting module helper: " + this.name);
+  start: function () {
+    Log.log(`Starting module helper: ${this.name}`);
   },
 
-
-  socketNotificationReceived: function(notification, payload) {
-    if (notification === 'CONFIG') {
+  socketNotificationReceived: function (notification, payload) {
+    if (notification === "CONFIG") {
       this.config = payload;
       this.collectData();
-      self = this;
+      let self = this;
       setInterval(function () {
         self.collectData();
       }, 60 * 1000);
@@ -22,29 +20,31 @@ module.exports = NodeHelper.create({
   },
 
   collectData: function () {
-    var today;
+    let today;
     if (moment() < moment(this.config.switchTime, "HH:mm")) {
-     today = moment().format("YYYY-MM-DD");
+      today = moment().format("YYYY-MM-DD");
     } else {
-     today = moment().add(1, "days").format("YYYY-MM-DD");
+      today = moment().add(1, "days").format("YYYY-MM-DD");
     }
-    var requestURL = 'https://openmensa.org/api/v2/canteens/'+this.config.canteen+'/days/'+today+'/meals';
-    console.log(requestURL);
-    var self = this;
-    https.get(requestURL, (response) => {
-      console.log('statusCode: ', response && response.statusCode);
-
-      response.on('data', (chunk) => {
-          if (response.statusCode == 404) {
-              console.log("Kantine hat heut dicht!");
-              self.sendSocketNotification("CLOSED", null);
-          } else {
-              console.log("Data: "+ JSON.parse(chunk));
-              self.sendSocketNotification("MEALS", JSON.parse(chunk));
-          }
-      });
-    }).on("error", (err) => {
-        console.log("Error: " + err.message);
-    });
+    const requestURL = `http://openmensa.org/api/v2/canteens/${this.config.canteen}/days/${today}/meals`;
+    // Log.log(requestURL);
+    let self = this;
+    request(
+      {
+        url: requestURL,
+        json: true
+      },
+      function (error, response, body) {
+        Log.log("MMM-Canteen - statusCode: ", response && response.statusCode);
+        if (error) {
+          Log.error(error);
+        } else if (response.statusCode === 404) {
+          Log.info("Kantine hat heut dicht!");
+          self.sendSocketNotification("CLOSED", null);
+        } else {
+          self.sendSocketNotification("MEALS", body);
+        }
+      }
+    );
   }
 });
